7e5a30d03e9bd74853d28d6ff48908768a1e39c9
Trang Pham <trpham@c1r4p3.hive.fi>
2024-11-25 18:04:11 +0200

    all

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,59 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 13:32:14 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/25 15:41:32 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/25 17:39:29 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	ft_putstr(char *s);
 
 char	*get_next_line(int	fd)
 {
 	ssize_t	bytes_read;
-	ssize_t	length;
-	static void	*buffer;
+	static char	*buffer;
+	int	i_buffer;
+	int	i_line;
+	char	*line = "";
 
 	buffer = malloc((BUFFER_SIZE ) + 1);
 	if (!buffer)
 		return (0);
-	length = 0;
+	i_buffer = 0;
+	i_line = 0;
 	bytes_read = -1;
 	while (bytes_read) // ==0 end of file
 	{
-		bytes_read = read(fd, buffer, sizeof(buffer));
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read == -1) // error in reading
 			return (NULL);
-		length += bytes_read;
+		buffer[bytes_read] = '\0';
+		if (buffer[i_buffer] && buffer[i_buffer] == '\n')
+		{
+			ft_putstr(line);
+			i_line = 0;
+		}
+		else
+		{
+			line[i_line] = buffer[i_buffer];
+			i_buffer++;
+			i_line++;
+		}
 		
 	}
 	// buffer[bytes_read] = '\0';
-	free(buffer);
-	return (0);
+	// free(buffer);
+	return (buffer);
 }
 
+void	ft_putstr(char *s)
+{
+	if (!s)
+		return ;
+	while (*s)
+	{
+		write(1, s, 1);
+		s++;
+	}
+}
--- test.c
+++ test.c
@@ -6,15 +6,44 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 12:37:33 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/25 12:58:45 by trpham           ###   ########.fr       */
+/*   Updated: 2024/11/25 17:25:36 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "stdio.h"
+#include <fcntl.h>
+#include <stdio.h>
 
 int	main(void)
 {
-	printf("visaolaithe %s\n", get_next_line(0));
+	char	*nextline = NULL;
+	int	fd;
+
+	// Create an open file description that refers to a file and 
+	// a file descriptor that refers to that open file description
+	fd = open("text.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error! Could not open file\n");
+		exit (-1);
+	}
+	while (nextline)
+	{
+		nextline = get_next_line(fd);
+		// if (!nextline)
+		// 	break ;
+		if (!nextline)
+		{
+			printf("Error! Could not get line\n");
+			// free(nextline);
+			return (-1);
+		}
+		printf("%s\n", nextline);
+		free(nextline);
+		
+	}
+	// free(nextline);
+	close(fd);
 	return (0);
+	
 }
