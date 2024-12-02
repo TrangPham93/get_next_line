7147ce29e72e76587ed7b27fa07bd631a37d1cfc
TrangPham93 <hatrangc2@gmail.com>
2024-12-02 09:21:00 +0200

    resubmit

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 10:43:27 by trpham            #+#    #+#             */
-/*   Updated: 2024/11/28 13:29:31 by trpham           ###   ########.fr       */
+/*   Updated: 2024/12/02 09:20:33 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,10 @@
 # endif
 
 char	*get_next_line(int fd);
-char	*read_file(char *buffer, int fd);
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 
 #endif
